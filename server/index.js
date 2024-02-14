7799f79a3cf7341bf98bbee20ce869e16aaa5f06
BillyLee720 <209410256@gms.tku.edu.tw>
2024-02-15 00:03:40 +0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser'); //解析請求資料(body)
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const multer = require('multer');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
+const { register } = require('module');
+const { createPost } = require('./controllers/posts');
+const { verifyToken } = require('./middleware/auth');
+const User = require('./models/User');
+const Post = require('./models/Post');
+const { users, posts } = require('./data/index');
+/* CONFIGURATIONS */
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/*FileStorage */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
+/* ROUTES */
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('posts', postRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port:${PORT}`));
+    /*ADD DATE */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
---
+++ server/models/Post.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      require: true,
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    location: {
+      type: String,
+    },
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    link: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
