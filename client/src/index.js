983079c60d6d4c3e31703b04fbf7e08b16ea946e
BillyLee720 <209410256@gms.tku.edu.tw>
2024-02-16 02:40:23 +0800

    add client page state

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import authReducer from './state';
+import { configureStore } from '@reduxjs/toolkit'; //用於創建Redux Store的函數
+import { Provider } from 'react-redux'; //將Redux Store用進React
+import {
+  persistStore, //創建持久化存儲的Redux Store
+  persistReducer, //配置持久化存儲的reducer
+  FLUSH, //FLUCH-REGISTER -> action types 用於控制持久化存儲的動作
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //是 Redux Persist 使用的儲存引擎，默認使用localStorage
+import { PersistGate } from 'redux-persist/integration/react'; //用於包裹React，確認數據恢復後才渲染
+
+const persistConfig = { key: 'root', storage, version: 1 };
+const persistReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
